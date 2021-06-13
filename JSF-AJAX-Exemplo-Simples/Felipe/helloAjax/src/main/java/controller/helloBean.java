baf08c8fe3aab3124f3cae876a9f24aaf520c89a
felipe <felipeluis.6@gmail.com>
2021-06-13 15:57:08 -0300

    jsf ajax, and jsf json

--- JSF-AJAX-Exemplo-Simples/Felipe/helloAjax/src/main/java/controller/helloBean.java
+++ JSF-AJAX-Exemplo-Simples/Felipe/helloAjax/src/main/java/controller/helloBean.java
@@ -8,12 +8,17 @@ package controller;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter; 
+import java.util.*;
 
 @Named(value = "helloBean")
 @SessionScoped
 public class helloBean implements Serializable {
 
-    private String name;
+    private String name;    
+    private String program;
+    private Date someDate;
 
     public helloBean() {
     }
@@ -21,15 +26,56 @@ public class helloBean implements Serializable {
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
+    public String getProgram() {
+        return program;
+    }
+
+    public void setProgram(String program) {
+        this.program = program;
+    }
+
+    public Date getSomeDate() {
+        return someDate;
+    }
+
+    public void setSomeDate(Date someDate) {
+        this.someDate = someDate;
+    }
+
     public String getSayWelcome() {
-        //check if null?
         if ("".equals(name) || name == null) {
             return "";
         } else {
-            return "Ajax message : Welcome " + name;
+            return "Welcome: " + name;
+        }
+    }
+
+    public String getSayProgram() {
+        String outStr = "";
+        
+        if ("".equals(program) || program == null) {
+            return "";
+        } else {
+            int num = Integer.parseInt(program);
+
+            switch (num) {
+                case 1:
+                    outStr = "Programa: Tecnologia para Desenvolvimento Web";
+                    break;
+                case 2:
+                    outStr = "Programa: Oficina Certificadora";
+                    break;
+                case 3:
+                    outStr = "Programa: Outra disciplina";
+                    break;
+            }
+
+            return outStr;
         }
     }
 }
